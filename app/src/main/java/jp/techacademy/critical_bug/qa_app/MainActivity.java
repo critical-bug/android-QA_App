0424fae9fb79bfbca03b189f23244a33723ecdc3
critical-bug <critical-bug@users.noreply.github.com>
2017-02-10 10:08:03 +0000

    メイン画面

--- app/src/main/java/jp/techacademy/critical_bug/qa_app/MainActivity.java
+++ app/src/main/java/jp/techacademy/critical_bug/qa_app/MainActivity.java
@@ -2,8 +2,12 @@ package jp.techacademy.critical_bug.qa_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -15,12 +19,15 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+    private int mGenre;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +40,37 @@ public class MainActivity extends AppCompatActivity {
                     Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                     startActivity(intent);
                 }
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, mToolbar, R.string.app_name, R.string.app_name);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull final MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.nav_hobby) {
+                    mToolbar.setTitle("趣味");
+                    mGenre = 1;
+                } else if (id == R.id.nav_life) {
+                    mToolbar.setTitle("生活");
+                    mGenre = 2;
+                } else if (id == R.id.nav_health) {
+                    mToolbar.setTitle("健康");
+                    mGenre = 3;
+                } else if (id == R.id.nav_compter) {
+                    mToolbar.setTitle("コンピューター");
+                    mGenre = 4;
+                }
+
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
             }
         });
     }
